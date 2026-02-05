e62d5aa5cea6cef77ed9ffbbf536b04b6690eed6
Jinson <jinson@unknown.home>
2026-02-05 16:53:21 +0000

    updates task 3

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const topicsRouter = require("./routes/topics.route");
 const articlesRouter = require("./routes/articles.route");
+const usersRouter = require("./routes/users.routes");
 const app = express();
 
 app.use("/api/topics", topicsRouter);
 app.use("/api/articles", articlesRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
---
+++ routes/users.routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { getUsers } = require("../controllers/users.controller");
+const usersRouter = express.Router();
+
+usersRouter.get("/", getUsers);
+
+module.exports = usersRouter;
