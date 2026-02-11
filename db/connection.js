ba077c772aaec25a832b1bd5d7b0bc521f1b3a4e
Jinson <jinson@unknown.home>
2026-02-11 09:14:29 +0000

    updates connection file

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,39 @@
+// const { Pool } = require("pg");
+
+// const ENV = process.env.NODE_ENV || "development";
+
+// const config = {};
+
+// if (ENV === "production") {
+//   config.connectionString = process.env.DATABASE_URL;
+//   config.max = 2;
+// }
+// module.exports = new Pool(config);
+
+// require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+
+// const db = new Pool();
+
+// if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+//   throw new Error("PGDATABASE or DATABASE_URL not set");
+// } else {
+//   console.log(`Connected to ${process.env.PGDATABASE}`);
+// }
+
+// module.exports = db;
+
 const { Pool } = require("pg");
+const path = require("path");
 
 const ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config({
+  path: path.join(__dirname, `../.env.${ENV}`),
+});
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
 
 const config = {};
 
@@ -8,16 +41,9 @@ if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
-module.exports = new Pool(config);
 
-require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+const db = new Pool(config);
 
-const db = new Pool();
+console.log(`Connected to ${process.env.PGDATABASE}`);
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
-} else {
-  console.log(`Connected to ${process.env.PGDATABASE}`);
-}
-
-// module.exports = db;
+module.exports = db;
